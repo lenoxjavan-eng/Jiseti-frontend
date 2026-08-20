3e60438188aad9ed67212bdd53f642f0908e227b
kiplimo <kiplimo.kiptoo@student.moringaschool.com>
2026-08-20 22:53:07 +0300

    fix: align lenox user and admin UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import AdminDashboard from './pages/AdminDashboard'
 import AdminRecordDetails from './pages/AdminRecordDetails'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import MyRecords from './pages/MyRecords'
+import CreateRedFlag from './pages/CreateRedFlag'
+import CreateIntervention from './pages/CreateIntervention'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/records" element={<MyRecords />} />
+        <Route path="/my-records" element={<MyRecords />} />
+        <Route path="/create/red-flag" element={<CreateRedFlag />} />
+        <Route path="/create/intervention" element={<CreateIntervention />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/record/:id" element={<AdminRecordDetails />} />
-        <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
       </Routes>
     </Router>
   )
